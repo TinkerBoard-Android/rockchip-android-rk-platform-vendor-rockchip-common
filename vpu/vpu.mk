de279e16116345903c2c2139555b1cf5f4d2cc49
sunny.he <heyun@rock-chips.com>
2017-12-29 15:18:06 +0800

    vpu: rk3126c 7.1: add libvpu/libmpp to PRODUCT_PACKAGES
    Change-Id: If9fb98fe654f0396bd10003f6c58d6b911644b9a
    
    Former-commit-id: d04b9349901eaae8ba2a2bfb715fdbed6efb5f98

--- vpu/vpu.mk
+++ vpu/vpu.mk
@@ -90,7 +90,7 @@ PRODUCT_PACKAGES += \
     iso
 endif
 
-ifneq ($(filter rk3228 rk3288 rk3328 rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
+ifneq ($(filter rk3228 rk3288 rk3328 rk3366 rk3399 rk3126c, $(strip $(TARGET_BOARD_PLATFORM))), )
 PRODUCT_PACKAGES += \
     libmpp \
     libvpu
